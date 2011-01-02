463a3fc31673bfb427fc7d199c559e1a5f0224e1
david simmons <david@david-laptop.(none)>
2011-01-02 11:11:29 +0000

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
                                            
+  has_many :microposts, :dependent => :destroy
+
   validates :name,  :presence   => true,
                     :length     => { :maximum => 50 }
                     
@@ -44,6 +46,11 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def feed
+    # This is prelimary. 
+    Micropost.where("user_id = ?", id)
   end
   
   private
